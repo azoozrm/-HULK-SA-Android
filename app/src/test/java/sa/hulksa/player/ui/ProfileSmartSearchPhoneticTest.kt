5b068e688e3e3433288ed06acdf54986d1c1fec9
azoozrm <116194689+azoozrm@users.noreply.github.com>
2026-08-14 05:24:38 +0300

    Add cross-script search matching tests

---
+++ app/src/test/java/sa/hulksa/player/ui/ProfileSmartSearchPhoneticTest.kt
@@ -0,0 +1,34 @@
+package sa.hulksa.player.ui
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Test
+
+class ProfileSmartSearchPhoneticTest {
+    @Test
+    fun arabicTransliterationMatchesPrisonBreak() {
+        assertEquals(0, crossScriptPhoneticRank("بريزون بريك", "prison break"))
+    }
+
+    @Test
+    fun partialArabicTransliterationMatchesEnglishTitlePrefix() {
+        assertEquals(1, crossScriptPhoneticRank("بريزون", "prison break"))
+    }
+
+    @Test
+    fun commonArabicTransliterationsMatchAcrossScripts() {
+        assertEquals(0, crossScriptPhoneticRank("بريكنق باد", "breaking bad"))
+        assertEquals(0, crossScriptPhoneticRank("قيم اوف ثرونز", "game of thrones"))
+    }
+
+    @Test
+    fun sameScriptDoesNotUseCrossScriptFallback() {
+        assertNull(crossScriptPhoneticRank("prison break", "prison break"))
+        assertNull(crossScriptPhoneticRank("بريزون بريك", "بريزون بريك"))
+    }
+
+    @Test
+    fun unrelatedCrossScriptTextDoesNotMatch() {
+        assertNull(crossScriptPhoneticRank("بريزون بريك", "the office"))
+    }
+}
