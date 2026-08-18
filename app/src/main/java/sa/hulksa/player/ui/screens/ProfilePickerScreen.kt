5fe1cee7eb0a02f79115258869b74b9f62ff8d2f
azoozrm <116194689+azoozrm@users.noreply.github.com>
2026-08-19 01:58:35 +0300

    Fix profile picker lint scope

--- app/src/main/java/sa/hulksa/player/ui/screens/ProfilePickerScreen.kt
+++ app/src/main/java/sa/hulksa/player/ui/screens/ProfilePickerScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -222,7 +221,7 @@ fun ProfilePickerScreen(
             return@Box
         }
 
-        BoxWithConstraints(
+        Box(
             modifier = Modifier
                 .fillMaxSize()
                 .safeDrawingPadding(),
