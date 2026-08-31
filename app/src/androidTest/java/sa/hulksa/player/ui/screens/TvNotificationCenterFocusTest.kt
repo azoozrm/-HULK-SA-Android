ddbb7c4042c8b6a141dfed54ceef2eed4ba4983d
azoozrm <116194689+azoozrm@users.noreply.github.com>
2026-08-31 17:03:25 +0300

    test: fix TV notification Compose API imports

--- app/src/androidTest/java/sa/hulksa/player/ui/screens/TvNotificationCenterFocusTest.kt
+++ app/src/androidTest/java/sa/hulksa/player/ui/screens/TvNotificationCenterFocusTest.kt
@@ -13,11 +13,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.test.assertDoesNotExist
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsFocused
 import androidx.compose.ui.test.assertIsNotFocused
-import androidx.compose.ui.test.fetchSemanticsNode
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performKeyInput
